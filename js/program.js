df6f89dd1e0b78596d538e281105415b14501369
Guillaume Bécan <guillaume.becan@irisa.fr>
2016-05-13 12:52:39 +0200

    favorites

--- js/program.js
+++ js/program.js
@@ -2,118 +2,51 @@ var modelsApp = angular.module("models-app", []);
 
 modelsApp.controller("ProgramController", function($scope) {
 
-    $scope.saved = "titi";
+
+    $scope.showFavorites = false;
 
     $scope.saveSession = function () {
         if(typeof(Storage) !== "undefined") {
-            localStorage.setItem("room", "toto");
-            $scope.saved = localStorage.getItem("room");
-            console.log($scope.saved);
+            localStorage.setItem("myCalendar", JSON.stringify($scope.days));
+            var myCalendar = JSON.parse(localStorage.getItem("myCalendar"));
+            console.log(myCalendar);
         }
-    }
+    };
 
-    $scope.roomsWorkshop = [
-        {
-            name: "Bouvet 1"
-        },
-        {
-            name: "Bouvet 2"
-        },
-        {
-            name: "Charcot"
-        },
-        {
-            name: "Vauban 1"
-        },
-        {
-            name: "Vauban 2"
-        },
-        {
-            name: "Lamannais 1"
-        },
-        {
-            name: "Lamannais 2"
-        },
-        {
-            name: "Lamannais 3"
-        },
-        {
-            name: "Rotonde Surcouf"
-        },
-        {
-            name: "Rotonde J. Cartier"
-        }
-    ];
 
-    $scope.roomsConference = [
-            {
-                name: "Chateaubriand"
-            },
-            {
-                name: "Lamannais 1+2"
-            },
-            {
-                name: "Lamannais 4+5"
-            },
-            {
-                name: "Grand large"
-            },
-            {
-                name: "Rotonde J. Cartier"
-            }
-        ];
-
-    $scope.days = [
+    $scope.data = [
         {
-            name: "Sunday",
-            sessions: [
+            name : "Monday",
+            rooms : ["Bouvet 1", "Bouvet 2"],
+            sessions : [
                 {
+                    name : "Farming session",
                     start: "9h",
-                    end: "10h45",
-                    rooms: [
-                        {
-                            name : "Bouvet 1",
-                            talks: [
-
-                            ]
-                        },
-                        {
-                            name : "Bouvet 2",
-                            talks: [
-
-                            ]
-                        },
-
-                    ]
-                },
-                {
-                    start: "11h15",
-                    end: "13h",
-                    rooms: [
-                        {
-                            name : "Bouvet 1",
-                            talks: [
-
-                            ]
-                        },
-                        {
-                            name : "Bouvet 2",
-                            talks: [
-                                {
-                                    title: "International Models and Evolution Workshop",
-                                    speakers: [
-                                        "Dalila Tamzalit",
-                                        "Alfonso Pierantonio",
-                                        "Bernhard Schaetz",
-                                        "Tanja Mayerhofer"
-                                    ]
-                                }
-                            ]
-                        }
+                    end: "10h30",
+                    talkGroups : [
+                        [
+                            { title: "MDE for cows"}, { title: "MDE for bulls"}
+                        ], [
+                            { title: "MDE for pigs"}, { title: "MDE or not MDE?"}
+                        ]
                     ]
                 }
             ]
         }
+    ];
 
-    ]
+
+    $scope.toggleFavorites = function() {
+
+    };
+
+
+    $scope.toggleFavoriteTalk = function(talk) {
+        // if (typeof talk.selected === "undefined") {
+        //     talk.selected = true;
+        // } else {
+            talk.selected=!talk.selected;
+        // }
+    }
+
 });
