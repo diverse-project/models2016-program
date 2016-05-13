721da20fd099691d9e70bb23d0f61ab2b2adcefb
Dorian Leroy <dorian.leroy@irisa.fr>
2016-05-13 17:47:46 +0200

    Changed data.js

--- js/data.js
+++ js/data.js
@@ -2,32 +2,59 @@ var data = [
 	{
 		name : "Monday",
 		rooms : [
+			"Bouvet 2",
 			"Bouvet 1",
-			"Bouvet 2"
+			"Charcot"
 		],
 		sessions : [
 			{
-				name : "Farming session",
+				name : "Farming session 1",
 				start : "9h",
 				end : "10h30",
 				talkGroups : [
 					[
 						{
-							title: "MDE for cows"
-						},
-						{
 							title: "MDE for bulls"
 						},
 						{
-							title: "MDE for me"
+							title: "MDE for pigs"
 						}
 					],
 					[
 						{
-							title: "MDE for pigs"
+							title: "MDE for cows"
 						},
 						{
 							title: "MDE or not MDE?"
+						}
+					],
+					[
+						{}
+					]
+				]
+			}
+			{
+				name : "Farming session 2",
+				start : "11h",
+				end : "12h30",
+				talkGroups : [
+					[
+						{}
+					],
+					[
+						{
+							title: "MDE for cows"
+						},
+						{
+							title: "MDE or not MDE?"
+						}
+					],
+					[
+						{
+							title: "MDE for bulls"
+						},
+						{
+							title: "MDE for pigs"
 						}
 					]
 				]
