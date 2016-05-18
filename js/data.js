d561e79cfa2c860206b56287d44cac6eb1e80b4a
Dorian Leroy <dorian.leroy@irisa.fr>
2016-05-18 17:26:20 +0200

    Updated js

--- js/data.js
+++ js/data.js
@@ -1,64 +1,284 @@
 var data = [
 	{
 		name : "Monday",
+		date : "20161007",
 		rooms : [
-			"Bouvet 1",
-			"Bouvet 2",
-			"Charcot"
-		],
-		sessions : [
 			{
-				name : "Farming session 1",
-				start : "9h",
-				end : "10h30",
-				talkGroups : [
-					[
-						{
-							title: "MDE for cows"
-						},
-						{
-							title: "MDE or not MDE?"
-						}
-					],
-					[
-						{
-							title: "MDE for bulls"
-						},
-						{
-							title: "MDE for pigs"
-						}
-					],
-					[
-						{}
-					]
-				]
+				name : "Vauban 1",
+				capacity : "150"
 			},
 			{
-				name : "Farming session 2",
-				start : "11h",
-				end : "12h30",
-				talkGroups : [
-					[
+				name : "Vauban 2",
+				capacity : "150"
+			}
+		],
+		sessionGroups : [
+			[
+				{
+					start : "22:00",
+					end : "23:00",
+					length : "4",
+					date : "20161007T220000Z",
+					events : [
 						{
-							title: "MDE for cows"
-						},
-						{
-							title: "MDE or not MDE?"
-						}
-					],
-					[
-						{}
-					],
-					[
-						{
-							title: "MDE for bulls"
-						},
-						{
-							title: "MDE for pigs"
+							type : "Lunch",
+							title : "lunch 1"
 						}
 					]
-				]
+				},
+				{}
+			],
+			[
+				{},
+				{
+					start : "18:00",
+					end : "19:00",
+					length : "4",
+					date : "20161007T180000Z",
+					events : [
+						{
+							type : "Lunch",
+							title : "lunch 1"
+						},
+						{
+							type : "Lunch",
+							title : "lunch 2"
+						}
+					]
+				}
+			],
+			[
+				{},
+				{
+					start : "19:00",
+					end : "21:00",
+					length : "8",
+					date : "20161007T190000Z",
+					events : [
+						{
+							type : "Workshop",
+							title : "WS-GEMOC",
+							organizers : [
+								"John Doe",
+								"Jane Doe"
+							]
+						}
+					]
+				}
+			],
+			[
+				{},
+				{
+					start : "21:00",
+					end : "22:00",
+					length : "4",
+					date : "20161007T210000Z",
+					events : [
+						{
+							type : "Tutorial",
+							title : "Tuto1",
+							organizers : [
+								"Dorian",
+								"fsergb"
+							]
+						}
+					]
+				}
+			],
+			[
+				{
+					start : "21:00",
+					end : "22:00",
+					length : "4",
+					date : "20161007T210000Z",
+					events : [
+						{
+							type : "Workshop",
+							title : "WS-GEMOC",
+							organizers : [
+								"John Doe",
+								"Jane Doe"
+							]
+						}
+					]
+				},
+				{}
+			],
+			[
+				{},
+				{
+					start : "19:00",
+					end : "21:00",
+					length : "8",
+					date : "20161007T190000Z",
+					events : [
+						{
+							type : "TalkSession",
+							title : "Stuff",
+							chair : "John Doe",
+							papers : [
+								{
+									title : "MDE for cows"
+									abstract : 
+									authors : [
+										"Jane Doe"
+									]
+								},
+								{
+									title : "MDE for pigs"
+									abstract : 
+									authors : [
+										"Pierre",
+										"Paul",
+										"Jacques"
+									]
+								}
+							]
+						}
+					]
+				}
+			]
+		]
+	},
+	{
+		name : "Tuesday",
+		date : "20161008",
+		rooms : [
+			{
+				name : "Vauban 2",
+				capacity : "150"
+			},
+			{
+				name : "Vauban 3",
+				capacity : "150"
 			}
+		],
+		sessionGroups : [
+			[
+				{
+					start : "22:00",
+					end : "23:00",
+					length : "4",
+					date : "20161008T220000Z",
+					events : [
+						{
+							type : "Lunch",
+							title : "lunch 1"
+						}
+					]
+				},
+				{}
+			],
+			[
+				{},
+				{
+					start : "18:00",
+					end : "19:00",
+					length : "4",
+					date : "20161008T180000Z",
+					events : [
+						{
+							type : "Lunch",
+							title : "lunch 1"
+						},
+						{
+							type : "Lunch",
+							title : "lunch 2"
+						}
+					]
+				}
+			],
+			[
+				{},
+				{
+					start : "19:00",
+					end : "21:00",
+					length : "8",
+					date : "20161008T190000Z",
+					events : [
+						{
+							type : "Workshop",
+							title : "WS-GEMOC",
+							organizers : [
+								"John Doe",
+								"Jane Doe"
+							]
+						}
+					]
+				}
+			],
+			[
+				{},
+				{
+					start : "21:00",
+					end : "22:00",
+					length : "4",
+					date : "20161008T210000Z",
+					events : [
+						{
+							type : "Tutorial",
+							title : "Tuto1",
+							organizers : [
+								"Dorian",
+								"fsergb"
+							]
+						}
+					]
+				}
+			],
+			[
+				{
+					start : "21:00",
+					end : "22:00",
+					length : "4",
+					date : "20161008T210000Z",
+					events : [
+						{
+							type : "Workshop",
+							title : "WS-GEMOC",
+							organizers : [
+								"John Doe",
+								"Jane Doe"
+							]
+						}
+					]
+				},
+				{}
+			],
+			[
+				{},
+				{
+					start : "19:00",
+					end : "21:00",
+					length : "8",
+					date : "20161008T190000Z",
+					events : [
+						{
+							type : "TalkSession",
+							title : "Stuff",
+							chair : "John Doe",
+							papers : [
+								{
+									title : "MDE for cows"
+									abstract : 
+									authors : [
+										"Jane Doe"
+									]
+								},
+								{
+									title : "MDE for pigs"
+									abstract : 
+									authors : [
+										"Pierre",
+										"Paul",
+										"Jacques"
+									]
+								}
+							]
+						}
+					]
+				}
+			]
 		]
 	}
 ]
