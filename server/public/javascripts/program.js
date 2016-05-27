17e349094f76dbe6518eed086be163add48fed23
Guillaume Bécan <guillaume.becan@irisa.fr>
2016-05-27 17:17:56 +0200

    prepare generation of ical on server

--- server/public/javascripts/program.js
+++ server/public/javascripts/program.js
@@ -102,88 +102,103 @@ modelsApp.controller("ProgramController", function($scope, $http) {
 
 
     ///// Export to iCal /////
-    function hash(string) {
-        var hash = 0;
-        if (string.length == 0) return hash;
-        for (i = 0; i < string.length; i++) {
-            var charI = string.charCodeAt(i);
-            hash = ((hash<<5)-hash)+charI;
-            hash = hash & hash;
-        }
-        return hash;
-    }
 
-
-    function createEvent(calendar, start, end, title, description, location) {
-        calendar.push("BEGIN:VEVENT");
-        calendar.push("DTSTART:" + start);
-        calendar.push("DTEND:" + end);
-        calendar.push("DTSTAMP:" + start);
-        calendar.push("ORGANIZER;CN=models2016-gc@inria.fr:mailto:models2016-gc@inria.fr");
-        calendar.push("UID:" + start + "-"  + hash(title) + "@models.irisa.fr");
-        calendar.push("DESCRIPTION:" + description); // TODO : max line is 75 characters
-        calendar.push("LOCATION:" + location);
-        calendar.push("SUMMARY:" + title); // TODO : max line is 75 characters
-        calendar.push("END:VEVENT");
-    }
-
-    $scope.exportToCal = function(favoritesOnly) {
-
-        // Create calendar
-        var calendar = [];
-        calendar.push("BEGIN:VCALENDAR");
-        calendar.push("VERSION:2.0");
-        calendar.push("PRODID:-//MODELS2016//Program");
-
-        $scope.data.forEach(function(day) {
-
-            day.sessionGroups.forEach(function(sessionGroup) {
-                if (sessionGroup.length > 0) {
-                    sessionGroup.forEach(function (session, roomIndex) {
-                        if (typeof session.events !== "undefined") {
-                            session.events.forEach(function (event, eventIndex) {
-
-                                if (typeof event.papers === "undefined") {
-
-                                    if (!favoritesOnly || ((typeof event.selected !== "undefined") && event.selected === true)) {
-                                        createEvent(calendar, session.icalStart, session.icalEnd, event.title, event.title, session.room); // TODO : description
-                                    }
-                                } else {
-                                    event.papers.forEach(function(talk, talkIndex) {
-                                        if (!favoritesOnly || ((typeof talk.selected !== "undefined") && talk.selected === true)) {
-                                            createEvent(calendar, talk.icalStart, talk.icalEnd, talk.title, talk.title, session.room); // TODO : description
-                                        }
-                                    });
-                                }
-
-
-                            });
-                        }
-                    });
-                }
-            });
-        });
-
-        calendar.push("END:VCALENDAR");
-        var file = calendar.join("\n");
-
-        // Download file
-        var blob = new Blob([file], {type: 'text/calendar'});
-        var inlinedDataUrl = 'data:text/calendar;charset=utf-8,' + encodeURIComponent(file);
-        var filename = "models.ics";
-        if(window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveBlob(blob, filename);
-        }
-        else{
+    $scope.generateIcal = function() {
+        $http.post("/generateIcal", $scope.data).then(function(response) {
             var elem = window.document.createElement('a');
-            // elem.href = window.URL.createObjectURL(blob);
-            elem.href = inlinedDataUrl;
-            elem.download = filename;
+            elem.href = "/getIcal/" + response.data;
+            elem.download = "models.ics";
+            console.log(elem);
             document.body.appendChild(elem);
             elem.click();
             document.body.removeChild(elem);
-        }
+        })
     };
+
+    // function hash(string) {
+    //     var hash = 0;
+    //     if (string.length == 0) return hash;
+    //     for (i = 0; i < string.length; i++) {
+    //         var charI = string.charCodeAt(i);
+    //         hash = ((hash<<5)-hash)+charI;
+    //         hash = hash & hash;
+    //     }
+    //     return hash;
+    // }
+    //
+    //
+    // function createEvent(calendar, start, end, title, description, location) {
+    //     calendar.push("BEGIN:VEVENT");
+    //     calendar.push("DTSTART:" + start);
+    //     calendar.push("DTEND:" + end);
+    //     calendar.push("DTSTAMP:" + start);
+    //     calendar.push("ORGANIZER;CN=models2016-gc@inria.fr:mailto:models2016-gc@inria.fr");
+    //     calendar.push("UID:" + start + "-"  + hash(title) + "@models.irisa.fr");
+    //     calendar.push("DESCRIPTION:" + description); // TODO : max line is 75 characters
+    //     calendar.push("LOCATION:" + location);
+    //     calendar.push("SUMMARY:" + title); // TODO : max line is 75 characters
+    //     calendar.push("END:VEVENT");
+    // }
+
+
+
+    // $scope.exportToCal = function(favoritesOnly) {
+    //
+    //     // Create calendar
+    //     var calendar = [];
+    //     calendar.push("BEGIN:VCALENDAR");
+    //     calendar.push("VERSION:2.0");
+    //     calendar.push("PRODID:-//MODELS2016//Program");
+    //
+    //     $scope.data.forEach(function(day) {
+    //
+    //         day.sessionGroups.forEach(function(sessionGroup) {
+    //             if (sessionGroup.length > 0) {
+    //                 sessionGroup.forEach(function (session, roomIndex) {
+    //                     if (typeof session.events !== "undefined") {
+    //                         session.events.forEach(function (event, eventIndex) {
+    //
+    //                             if (typeof event.papers === "undefined") {
+    //
+    //                                 if (!favoritesOnly || ((typeof event.selected !== "undefined") && event.selected === true)) {
+    //                                     createEvent(calendar, session.icalStart, session.icalEnd, event.title, event.title, session.room); // TODO : description
+    //                                 }
+    //                             } else {
+    //                                 event.papers.forEach(function(talk, talkIndex) {
+    //                                     if (!favoritesOnly || ((typeof talk.selected !== "undefined") && talk.selected === true)) {
+    //                                         createEvent(calendar, talk.icalStart, talk.icalEnd, talk.title, talk.title, session.room); // TODO : description
+    //                                     }
+    //                                 });
+    //                             }
+    //
+    //
+    //                         });
+    //                     }
+    //                 });
+    //             }
+    //         });
+    //     });
+    //
+    //     calendar.push("END:VCALENDAR");
+    //     var file = calendar.join("\n");
+    //
+    //     // Download file
+    //     var blob = new Blob([file], {type: 'text/calendar'});
+    //     var inlinedDataUrl = 'data:text/calendar;charset=utf-8,' + encodeURIComponent(file);
+    //     var filename = "models.ics";
+    //     if(window.navigator.msSaveOrOpenBlob) {
+    //         window.navigator.msSaveBlob(blob, filename);
+    //     }
+    //     else{
+    //         var elem = window.document.createElement('a');
+    //         // elem.href = window.URL.createObjectURL(blob);
+    //         elem.href = inlinedDataUrl;
+    //         elem.download = filename;
+    //         document.body.appendChild(elem);
+    //         elem.click();
+    //         document.body.removeChild(elem);
+    //     }
+    // };
 
     ///// Info on talk /////
     $scope.getInfo = function(talk, date) {
@@ -192,15 +207,5 @@ modelsApp.controller("ProgramController", function($scope, $http) {
     };
 
 
-    $scope.test = function() {
-        $http.post("/generateIcal", $scope.data).then(function(response) {
-            var elem = window.document.createElement('a');
-            elem.href = "/getIcal/" + response.data;
-            console.log(elem.href)
-            elem.download = "models.ics";
-            document.body.appendChild(elem);
-            elem.click();
-            document.body.removeChild(elem);
-        })
-    }
+
 });
